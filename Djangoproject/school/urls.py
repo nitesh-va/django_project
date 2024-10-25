b158e02a4bd854882c6150d17a77e6de40218c82
Nitesh Kumar Sahu <nitesh.kumar@vaisesika.local>
2024-10-25 16:34:38 +0530

    just updating

--- Djangoproject/school/urls.py
+++ Djangoproject/school/urls.py
@@ -4,5 +4,5 @@ from .views import SchoolView,SchoolViewByID
 urlpatterns = [
     path('',SchoolView.as_view(),name='school'),
     path('<int:school_id>/', SchoolView.as_view(),name='school_ID'),
-    path('school/<int:school_id>/', SchoolViewByID.as_view(), name='school_ID')
+    path('school/<int:school_id>/', SchoolViewByID.as_view(), name='school_ID'),
 ]
